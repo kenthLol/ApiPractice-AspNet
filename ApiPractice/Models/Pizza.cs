81a94ac2b753213dc609d085e9fa5f7229f5c9d6
Kenneth Lola <kenthsamu@gmail.com>
2024-02-19 20:36:27 -0600

    feat: Added Pizza Model and Pizza Service

---
+++ ApiPractice/Models/Pizza.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiPractice.Models;
+
+public class Pizza
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsGlutenFree { get; set; }
+}
---
+++ ApiPractice/Services/PizzaService.cs
@@ -0,0 +1,46 @@
+﻿namespace ApiPractice.Services;
+
+using ApiPractice.Models;
+
+public class PizzaService
+{
+    public static List<Pizza>? Pizzas { get; } 
+    static int nextId = 3;
+
+    static PizzaService()
+    {
+        Pizzas = new List<Pizza>
+        {
+            new Pizza { Id = 1, Name = "Cheese", IsGlutenFree = false },
+            new Pizza { Id = 2, Name = "Pepperoni", IsGlutenFree = true } 
+        };
+    }
+
+    public static List<Pizza> GetAll() => Pizzas!;
+
+    public static Pizza? Get(int id) => Pizzas!.FirstOrDefault(p => p.Id == id);
+
+    public static void Add(Pizza pizza)
+    {
+        pizza.Id = nextId++;
+        Pizzas!.Add(pizza);
+    }
+
+    public static void Remove(int id)
+    {
+        var pizza = Get(id);
+
+        if (pizza is null) return;
+
+        Pizzas!.Remove(pizza);
+    }
+
+    public static void Update(Pizza pizza)
+    {
+        var index = Pizzas!.FindIndex(p => p.Id == pizza.Id);
+
+        if (index == -1) return;
+
+        Pizzas[index] = pizza;
+    }
+}
